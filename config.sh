d146f901a1693b257d3e6c01c5f57b5d77faddad
FriendDementor <vanninipablo@gmail.com>
2021-07-05 01:02:12 -0300

    Update config.sh

--- config.sh
+++ config.sh
@@ -17,6 +17,7 @@ printf $timestamp"\n"$timestamp | passwd dementor
 sed -i 's/# %wheel ALL=(ALL) NOPASSWD: ALL/%wheel ALL=(ALL) NOPASSWD: ALL/g' /etc/sudoers
 
 ## configure sshd daemon
+sed -i 's/PermitRootLogin yes/PermitRootLogin no/g' /etc/ssh/sshd_config
 sed -i 's/#Port 22/Port 42686/g' /etc/ssh/sshd_config
 sed -i 's/ListenStream=22/ListenStream=42686/g' /etc/systemd/system/sshd.socket
 sed -i 's/UsePrivilegeSeparation sandbox/#UsePrivilegeSeparation sandbox/g' /etc/ssh/sshd_config
